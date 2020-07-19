f53f272230fa912a17b56ec0e46dc224f4b96472
Leva <smj510@yandex.ru>
2020-07-19 16:45:20 +0300

    can create pptx file

--- pptx.py
+++ pptx.py
@@ -1,1 +1,13 @@
-print('Hello World')
+import sys
+import os
+
+folder = 'C:\\Users\\smj51\\OneDrive\\Рабочий стол\\presentations\\'
+
+def pptx():
+    print(sys.argv)
+    fileName = str(sys.argv[1]) + str('.pptx')
+    open(folder + fileName, 'a').close()
+    print(fileName)
+
+if __name__ == '__main__':
+    pptx()
