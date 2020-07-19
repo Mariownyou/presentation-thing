33087f7b0d2a0ba98652108dd3afd7b519fe51be
Leva <smj510@yandex.ru>
2020-07-19 17:42:45 +0300

    working

--- pptx.py
+++ pptx.py
@@ -1,13 +1,29 @@
 import sys
 import os
 
-folder = 'C:\\Users\\smj51\\OneDrive\\Рабочий стол\\presentations\\'
 
-def pptx():
-    print(sys.argv)
-    fileName = str(sys.argv[1]) + str('.pptx')
-    open(folder + fileName, 'a').close()
-    print(fileName)
+class pptxAutomator:
+    mainFolder = 'C:\\Users\\smj51\\OneDrive\\Рабочий стол\\presentations\\'
+    default = 'C:\\Users\\smj51\\OneDrive\\Рабочий стол\\presentations\\all\\'
+    name = sys.argv[1]
+    folder = sys.argv[2] + '\\'
+
+    def createFilePath(self):
+        fileName = str(self.name) + str('.pptx')
+        return self.mainFolder + self.folder + self.name + '\\' +  fileName
+
+    def createFolderPath(self):
+        return self.mainFolder + self.folder + self.name
+
+    def createPptx(self):
+        os.mkdir(self.createFolderPath())
+        os.mkdir(self.createFolderPath()+'\\img')
+        open(self.createFilePath(), 'a').close()
+        print('Ur folder is succesfuly created ;)')
 
 if __name__ == '__main__':
-    pptx()
+    pptx = pptxAutomator()
+    command = str(sys.argv[0])
+
+    if command == 'pptx.py':
+        pptx.createPptx()
